55feab7fd987e95871a8f3544b6ddc92239808d7
Candace Savonen <cansav09@gmail.com>
2023-10-05 09:50:40 -0400

    I have a less bad way to handle file paths

--- scripts/save_copy.R
+++ scripts/save_copy.R
@@ -1,10 +1,21 @@
 #!/usr/bin/env Rscript
 
-# This script collects all the quiz responses and puts them in one sheet
+root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
 
-## Set up google credentials
-googledrive::drive_auth(path = Sys.getenv("GOOGLE_AUTH_CREDS"))
-
+authenticate <- function() {
+    googledrive::drive_deauth()
+    token <- googledrive::drive_auth(
+      email = "cansav09@gmail.com",
+      path = NULL,
+      scopes = "https://www.googleapis.com/auth/drive",
+      # Get new token if it doesn't exist
+      cache = ".secrets/",
+      use_oob = FALSE,
+      token = NULL
+    )
+    token <- drive_token() 
+    
+}
 
 # Load the libraries
 library(tidyverse)
@@ -18,22 +29,50 @@ drive_ids <- list(
   "https://drive.google.com/drive/folders/0AMoBC40Yf2maUk9PVA"
   )
 
+drive_id <- drive_ids[[1]]
+drive_info <- drive_get(as_id(drive_id))
 
-copy_the_drive <- function() {
+drive_folder_name <- paste0(lubridate::today(), drive_info$name, "-copy")
 
-  folder_id <- drive_get(as_id(drive_id))
+dir.create(drive_folder_name , showWarnings = FALSE)
 
-  # all_files_df <- drive_find(shared_drive = as_id(drive_id))
+# Set up folder
+all_files <- googledrive::drive_ls(as_id(drive_id), recursive = TRUE)
+all_files$type <- sapply(all_files$drive_resource, function(id) id$mimeType)
+all_files$parents <- sapply(all_files$drive_resource, function(id) unlist(id$parents))
+all_files$parent_name <- sapply(all_files$parents, function(id) googledrive::drive_get(as_id(id))$name)
+all_files$full_file_path <-  all_files$name
 
-  #find files in folder
-  all_files <- drive_find(shared_drive = as_id(drive_id), type = "files")
+# Extract only folder names 
+all_folder_ids <- all_files %>% 
+  dplyr::filter(type == "application/vnd.google-apps.folder")  %>% 
+  dplyr::pull(id)
 
-  dir.create(paste0(lubridate::today(), "file-copies"), showWarnings = FALSE)
 
-  # download them all but no folder structure
-  lapply(all_files$id, function(file) {
-      try({drive_download(file, path = )})
-  })
+## Track down what the file path name is for each file
+for (folder_id in all_folder_ids) {
+  
+  grandparent_filepath <- all_files %>% 
+    dplyr::filter(name == folder) %>% 
+    dplyr::pull(full_file_path) 
 
-  lubridate::today()
+  all_files <- all_files %>% 
+    dplyr::mutate(full_file_path = dplyr::case_when( 
+      folder_id == parents ~ file.path(grandparent_filepath, full_file_path), 
+      TRUE ~ full_file_path
+      ))
 }
+
+# Now create all the folders
+all_folder_paths <- all_files %>% 
+  dplyr::filter(type == "application/vnd.google-apps.folder")  %>% 
+  dplyr::pull(full_file_path) 
+
+# Create them all
+sapply(unique(all_folder_paths), dir.create, recursive = TRUE, showWarning = FALSE)
+
+
+try(googledrive::drive_download(as_id(file_id), overwrite = TRUE)))
+
+
+
